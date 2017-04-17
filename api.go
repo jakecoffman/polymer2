5f8f492c3c5ec37a273d2edd214fd365e5394c14
jake <jake@jakecoffman.com>
2017-04-16 21:25:54 -0500

    updated data and queries

--- api.go
+++ api.go
@@ -5,6 +5,8 @@ import (
 	"log"
 	"os"
 	"encoding/json"
+	_ "github.com/mattn/go-sqlite3"
+	"github.com/jmoiron/sqlx"
 )
 
 var books map[string][]string
@@ -30,26 +32,86 @@ func init() {
 	}
 }
 
+type Book struct {
+	BookNumber int `db:"book_number"`
+	ShortName string `db:"short_name"`
+	LongName string `db:"long_name"`
+}
+
 func main() {
+	esvdb := mustOpen("./data/ESV.SQLite3")
+	msgdb := mustOpen("./data/MSG.SQLite3")
+	defer esvdb.Close()
+	defer msgdb.Close()
+
+	versionMap := map[string]*sqlx.DB{
+		"esv": esvdb,
+		"msg": msgdb,
+	}
+
 	r := gin.Default()
 	r.NoRoute(func (c *gin.Context) {
 		c.JSON(404, gin.H{"error": "not found"})
 	})
+	// list books
 	r.GET("/books", func(c *gin.Context) {
+		var books []Book
+		err := esvdb.Select(&books, "select book_number, short_name, long_name from books order by book_number")
+		if err != nil {
+			c.JSON(500, map[string]string{"error": err.Error()})
+			return
+		}
 		c.JSON(200, books)
 	})
-	r.GET("/books/:book", func(c *gin.Context) {
-		book := MSG[c.Param("book")]
+	// list chapters
+	r.GET("/versions/:version/:book", func(c *gin.Context) {
+		version := c.Param("version")
+		selectedDb, ok := versionMap[version]
+		if !ok {
+			c.JSON(422, gin.H{"error": "bad version string"})
+			return
+		}
+		book := c.Param("book")
 		chapters := []string{}
-		for c := range book {
-			chapters = append(chapters, c)
+		err := selectedDb.Select(&chapters, "select distinct chapter from verses where book_number=?", book)
+		if err != nil {
+			c.JSON(500, gin.H{"error": err.Error()})
+			return
 		}
 		c.JSON(200, chapters)
 	})
-	r.GET("/books/:book/:chapter", func(c *gin.Context) {
-		c.JSON(200, MSG[c.Param("book")][c.Param("chapter")])
+	// list verses of chapter
+	r.GET("/versions/:version/:book/:chapter", func(c *gin.Context) {
+		version := c.Param("version")
+		book := c.Param("book")
+		chapter := c.Param("chapter")
+		selectedDb, ok := versionMap[version]
+		if !ok {
+			c.JSON(422, gin.H{"error": "bad version string"})
+			return
+		}
+		var verses []Verse
+		err := selectedDb.Select(&verses, "select verse, text from verses where book_number=? and chapter=?", book, chapter)
+		if err != nil {
+			c.JSON(500, gin.H{"error": err.Error()})
+			return
+		}
+		c.JSON(200, verses)
 	})
 	port := "8111"
 	log.Println("Serving http://localhost:" + port)
 	r.Run("0.0.0.0:" + port)
+}
+
+type Verse struct {
+	Verse int `db:"verse"`
+	Text string `db:"text"`
+}
+
+func mustOpen(dbname string) *sqlx.DB {
+	ddb, err := sqlx.Open("sqlite3", dbname)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return ddb
 }
